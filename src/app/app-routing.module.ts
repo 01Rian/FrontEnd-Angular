2ab10a4c56e08f6500ba7ad4c3c0ae4201828750
Rian <riansantosjr11@gmail.com>
2024-02-09 18:21:03 -0300

    add routes paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoleComponent } from './Components/role/role.component';
+import { UserComponent } from './Components/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "users" , component : UserComponent},
+  {path: "roles", component : RoleComponent},
+  {path: "", redirectTo: '/users', pathMatch: "full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
